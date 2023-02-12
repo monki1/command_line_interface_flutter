79627e0dc9a3c10ff5d672dc3c5dae3e535d7c53
monki1 <kxye111@gmail.com>
2023-02-11 20:06:08 -0500

    export

--- lib/command_line_interface.dart
+++ lib/command_line_interface.dart
@@ -1,3 +1,4 @@
 export 'package:command_line_interface/src/ui_controller/ui_controller.dart';
 export 'package:command_line_interface/src/cli_controller.dart';
+export 'package:command_line_interface/src/util/util.dart';
 
---
+++ lib/src/util/util.dart
@@ -0,0 +1,3 @@
+export 'package:command_line_interface/src/util/command_node.dart';
+export 'package:command_line_interface/src/util/terminal_scope.dart';
+export 'package:command_line_interface/src/util/terminal.dart';
