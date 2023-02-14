67ee9a6fc42a20b702ae96a273aeda41620f920b
monki1 <kxye111@gmail.com>
2023-02-14 12:22:59 -0500

    scroll up & down

--- lib/src/util/cli_node.dart
+++ lib/src/util/cli_node.dart
@@ -1,34 +1,47 @@
 import 'package:command_line_interface/command_line_interface.dart';
 import 'package:flutter/cupertino.dart';
 
-
 abstract class CLINode {
   late CLIController controller;
   late CLIScope scope;
   CLINode();
 
-  getAdopted(CLINode node_){
+  getAdopted(CLINode node_) {
     controller = node_.controller;
     scope = node_.scope;
   }
-  set dropText(String s){
-    controller.input.textEditingController.text = s; //set the text in text field
-    moveInsertionPoint =controller.input.textEditingController.text.length; // text insertion point moved to the end
-    controller.input.focusNode.requestFocus(); //this is to make sure the focus is on the input
+
+  set dropText(String s) {
+    controller.input.textEditingController.text =
+        s; //set the text in text field
+    moveInsertionPoint = controller.input.textEditingController.text
+        .length; // text insertion point moved to the end
+    controller.input.focusNode
+        .requestFocus(); //this is to make sure the focus is on the input
   }
-  set moveInsertionPoint(int i){
-    controller.input.textEditingController.selection = TextSelection.fromPosition(TextPosition(offset: i));
+
+  set moveInsertionPoint(int i) {
+    controller.input.textEditingController.selection =
+        TextSelection.fromPosition(TextPosition(offset: i));
+  }
+
+  scrollDown() {
+    controller.display.scrollController.animateTo(
+      controller.display.scrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
+  }
+
+  scrollUp() {
+    controller.display.scrollController.animateTo(
+      controller.display.scrollController.position.minScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
   }
 }
 
-abstract class FunctionNode extends CLINode{
+abstract class FunctionNode extends CLINode {
   FunctionNode();
-
-
-
-
 }
-
-
-
-
