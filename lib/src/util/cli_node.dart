9b7365eb7abb9aad9c8f828741d77a334a65f9ef
monki1 <kxye111@gmail.com>
2023-02-14 11:15:08 -0500

    request focus when drop text to input TextField

--- lib/src/util/cli_node.dart
+++ lib/src/util/cli_node.dart
@@ -12,8 +12,9 @@ abstract class CLINode {
     scope = node_.scope;
   }
   set dropText(String s){
-    controller.input.textEditingController.text = s;
-    moveInsertionPoint =controller.input.textEditingController.text.length;
+    controller.input.textEditingController.text = s; //set the text in text field
+    moveInsertionPoint =controller.input.textEditingController.text.length; // text insertion point moved to the end
+    controller.input.focusNode.requestFocus(); //this is to make sure the focus is on the input
   }
   set moveInsertionPoint(int i){
     controller.input.textEditingController.selection = TextSelection.fromPosition(TextPosition(offset: i));
