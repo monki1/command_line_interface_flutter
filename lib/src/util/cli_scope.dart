67d986d75dad4a6e0a6055b69cd824d542db4939
monki1 <kxye111@gmail.com>
2023-02-14 11:20:37 -0500

    subscriber can be identified with id (string)

--- lib/src/util/cli_scope.dart
+++ lib/src/util/cli_scope.dart
@@ -4,7 +4,10 @@ class CLIScope {
   //get the text editing controller
   final Function(String) _rootInterpreter;
   final Function(String)? _rootListener;
-  final List<List<Function(String)>> _suscribers = [];
+  // final List<List<Function(String)>> _suscribers = [];
+  final List<String> _subscriberIds = [];
+  final Map<String, List<Function(String)>> _subscribersMap = {};
+
   bool Function(String)? _topListener;
   bool Function(String)? _topInterpreter;
 
@@ -12,49 +15,61 @@ class CLIScope {
       : _rootListener = rootListener;
 
   setTop(
-      {bool Function(String)? interpreter, bool Function(String)? changeListener}) {
-    _topInterpreter = interpreter;
-    _topListener = changeListener;
+    // / top functions :[@param topInterpreter]: the function that will be called when the input is recieved
+    ///   if the function returns true, then the input is consumed and no other function will recieve the input
+    /// @param topChangeListener: the function that will be called when the input is recieved
+    /// are the first to recieve the input
+    ///   to stop the input from being passed down to the next layer
+    ///  return true
+      {bool Function(String)? topInterpreter, bool Function(String)? topChangeListener}) {
+    _topInterpreter = topInterpreter;
+    _topListener = topChangeListener;
   }
 
-  Function request(Function(String) interpreter, {Function(String)? changeListener}) {
+  Function request(Function(String) interpreter, {Function(String)? changeListener, String? id ///id should be unique, used to identify the subscriber layer.
+  ///this function returns a function releases this subscriber layer 
+  }) {
+    id = id?? DateTime.now().millisecondsSinceEpoch.toString();
     ///requests the interpreter to be the active interpreter
     List<Function(String)> input = [interpreter]+(changeListener!=null?[changeListener]:[]);
-    _suscribers.insert(0, input);
+    _subscribersMap[id] = input;
+    _subscriberIds.insert(0, id);
+    // _suscribers.insert(0, input);
     return (){
       // release(interpreter, changeListener: changeListener);
+      releaseSubscriberWithId(id!);
     };
   }
 
   releaseActive() {
     ///releases the active interpreter
-    if (_suscribers.isNotEmpty) {
-      _suscribers.removeAt(0);
+    if (_subscriberIds.isNotEmpty) {
+      // _suscribers.removeAt(0);
+      String id = _subscriberIds[0];
+      _subscriberIds.removeAt(0);
+      _subscribersMap.remove(id);
     }
   }
 
-  // release(Function(String) interpreter, {Function(String)? changeListener}) {
-  //   List<Function(String)> input = [interpreter]+(changeListener!=null?[changeListener]:[]);
-  //   _suscribers.remove(input);
-  // }
-
-  
+  releaseSubscriberWithId(String id){
+    if(_subscriberIds.contains(id)){
+      _subscriberIds.remove(id);
+      _subscribersMap.remove(id);
+    }
+  }
 
   interpret(String s) {
     ///conntect this to incoming text onSubmit
     ///entry point for the input
     if (_topInterpreter != null) {
       if (_topInterpreter!(s)) {
-        ///if the top interpreter returns false, then the input is consumed
+        ///if the top interpreter returns true, then the input is consumed
         return;
       }
     }
-    if (_suscribers.isNotEmpty) {
-      _suscribers[0][0](s);
-
-
-
-
+    if (_subscriberIds.isNotEmpty) {
+      _subscribersMap[_subscriberIds[0]]![0](s);
+      /// ^ if there is no subscriber
     } else {
       _rootInterpreter(s);
     }
@@ -64,27 +79,49 @@ class CLIScope {
     ///optional: connect this to incoming text onChange
     if (_topListener != null) {
       if (_topListener!(s)) {
-        ///if the top listener returns false, then the input is consumed
+        ///if the top listener returns true, then the input is consumed
         return;
       }
     }
-    if (_suscribers.isNotEmpty) {
+    if (_subscriberIds.isNotEmpty) {
 
-      //for i, if the item.length==2 then 
-      for (int i = 0; i < _suscribers.length; i++) {
-        if(_suscribers[i].length==2){
-          _suscribers[i][1](s);
-        }
+      int k = 0;
+    bool foundListener = false;
+
+    while (k < _subscriberIds.length && !foundListener) {
+      String id = _subscriberIds[k];
+      List<Function(String)> layer = _subscribersMap[id]!;
+      if (layer.length == 2) {
+        foundListener = true;
+        layer[1](s);
       }
+      k++;
+    }
+
     } else {
       _rootListener!(s);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   }
 
   bool isActive(Function(String) interpreter) {
     ///checks if the interpreter is the active interpreter
-    if (_suscribers.isNotEmpty) {
-      return _suscribers[0][0] == interpreter;
+    if (_subscriberIds.isNotEmpty) {
+      return _subscribersMap[_subscriberIds[0]]![0] == interpreter;
+
     }
     return false;
     
