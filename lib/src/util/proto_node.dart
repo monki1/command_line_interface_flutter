a7417ea7c05058a499eca49ef0c3e1bd5f8dbf33
monki1 <kxye111@gmail.com>
2023-02-14 11:45:58 -0500

    release scope

--- lib/src/util/proto_node.dart
+++ lib/src/util/proto_node.dart
@@ -1,47 +1,51 @@
-
 import 'package:command_line_interface/command_line_interface.dart';
 
 abstract class ProtoNode extends CLINode {
   List<CommandNode> nodes = [];
 
-  Function(String)? listenToChange;
   interpret(String s) async {
-    if(!(await letNodesInterpret(s)) || s.isEmpty){
+    ///the opinion is that
+    if (!(await letNodesInterpret(s)) || s.isEmpty) {
       loadScreen();
     }
   }
 
+  listenToChange(String s) {}
+
   Future<bool> letNodesInterpret(String s) async {
-    for(CommandNode node in nodes){
-      if(node.isValidInput(s)){
+    for (CommandNode node in nodes) {
+      if (node.isValidInput(s)) {
         return await node.interpret(s);
       }
     }
     return false;
   }
 
-  void add(CommandNode node){
+  void add(CommandNode node) {
     node.getAdopted(this);
-    for(CommandNode i in node.nodes){
+    for (CommandNode i in node.nodes) {
       i.getAdopted(this);
     }
     nodes.add(node);
   }
 
-  void loadScreen(){}
+  void loadScreen() {}
 
   bool get isActiveScope => scope.isActive(interpret);
   // releaseScope(){
   //   scope.release(interpret);
   // }
-  requestScope(){
-    scope.request(interpret, changeListener: listenToChange);
+  requestScope() {
+    _releaseScope = scope.request(interpret, changeListener: listenToChange);
   }
-  
 
+  Function? _releaseScope;
 
-
-
-
+  releaseScope() {
+    if (_releaseScope != null) {
+      releaseScope();
+      dropText = ""; //empty str calls loadScreen of next in queue
+      _releaseScope = null;
+    }
+  }
 }
-
