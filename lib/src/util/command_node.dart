800f85cc5d89fb2d36c186013e944199ee3ca1b7
monki1 <kxye111@gmail.com>
2023-02-14 22:14:35 -0500

    CommandNode getAdopted run getAdopted on each node

--- lib/src/util/command_node.dart
+++ lib/src/util/command_node.dart
@@ -1,7 +1,13 @@
 import 'package:command_line_interface/command_line_interface.dart';
 
-abstract class CommandNode  extends ProtoNode{
-  
+abstract class CommandNode extends ProtoNode {
   bool isValidInput(String s);
 
+  @override
+  getAdopted(CLINode node_) {
+    //pass down adoption
+    for (CommandNode cn in nodes) {
+      cn.getAdopted(node_);
+    }
+  }
 }
