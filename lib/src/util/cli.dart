1a801720281fc1af502f23110599d7e9aa71bc89
monki1 <kxye111@gmail.com>
2023-02-16 06:10:19 -0500

    cli can init with or without scope

--- lib/src/util/cli.dart
+++ lib/src/util/cli.dart
@@ -3,8 +3,19 @@ import 'package:command_line_interface/command_line_interface.dart';
 abstract class CLI extends ProtoNode {
   Function? releaseParentScope;
 
-  CLI(CLIController controller_) {
+  // CLI({ required super.controller ,required super.scope}) {
+  // }
+  // CLI.newScope({required super.controller}) {
+  //   scope = CLIScope(interpret, changeListener: listenToChange);
+  // }
+
+  CLI({required controller_, required CLIScope scope_}) {
     controller = controller_;
-    scope = CLIScope(interpret, rootListener: listenToChange);
+    scope = scope_;
+  }
+  CLI.withScope({required controller_}) {
+    controller = controller_;
+    scope = CLIScope(
+        rootSubmitListener: interpret, rootChangeListener: listenToChange);
   }
 }
